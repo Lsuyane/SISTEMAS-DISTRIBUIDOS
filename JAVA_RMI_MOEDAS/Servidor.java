e986ffce5f5e54e7276f0b1e8abba426aa57634a
Lsuyane <92032032+Lsuyane@users.noreply.github.com>
2024-02-08 11:33:41 -0300

    Update Servidor.java

--- JAVA_RMI_MOEDAS/Servidor.java
+++ JAVA_RMI_MOEDAS/Servidor.java
@@ -8,25 +8,21 @@ public class Servidor extends UnicastRemoteObject implements Conversao {
 
     @Override
     public double convertEuroToReal(double euroAmount) throws RemoteException {
-        // Simplesmente retornando o dobro para fins de exemplo
         return euroAmount * 2;
     }
 
     @Override
     public double convertRealToEuro(double realAmount) throws RemoteException {
-        // Simplesmente retornando a metade para fins de exemplo
         return realAmount / 2;
     }
 
     @Override
     public double convertDollarToReal(double dollarAmount) throws RemoteException {
-        // Simplesmente retornando o dobro para fins de exemplo
         return dollarAmount * 2;
     }
 
     @Override
     public double convertRealToDollar(double realAmount) throws RemoteException {
-        // Simplesmente retornando a metade para fins de exemplo
         return realAmount / 2;
     }
 
@@ -34,7 +30,7 @@ public class Servidor extends UnicastRemoteObject implements Conversao {
         try {
             Servidor server = new Servidor();
             java.rmi.registry.LocateRegistry.createRegistry(1099);
-            java.rmi.Naming.rebind("CurrencyConverterService", server);
+            java.rmi.Naming.rebind("Serviço de Conversao", server);
             System.out.println("Servidor pronto para conversão de moedas...");
         } catch (Exception e) {
             e.printStackTrace();
