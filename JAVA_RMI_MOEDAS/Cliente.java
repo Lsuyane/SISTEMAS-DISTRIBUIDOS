08f074783eb7288f1ca52bfde9d45a4f3270844c
Lsuyane <92032032+Lsuyane@users.noreply.github.com>
2024-02-08 11:40:26 -0300

    Update Cliente.java

--- JAVA_RMI_MOEDAS/Cliente.java
+++ JAVA_RMI_MOEDAS/Cliente.java
@@ -3,18 +3,18 @@ import java.rmi.Naming;
 public class Cliente {
     public static void main(String[] args) {
         try {
-            Conversao currencyConverter = (Conversao) Naming.lookup("rmi://localhost:1099/CurrencyConverterService");
+            Conversao Converter = (Conversao) Naming.lookup("rmi://localhost:1099/CurrencyConverterService");
 
             double euroAmount = 100;
             double realAmount = 200;
 
-            System.out.println("Conversão de Euro para Real: " + currencyConverter.convertEuroToReal(euroAmount));
-            System.out.println("Conversão de Real para Euro: " + currencyConverter.convertRealToEuro(realAmount));
+            System.out.println("Conversão de Euro para Real: " + Converter.convertEuroToReal(euroAmount));
+            System.out.println("Conversão de Real para Euro: " + Converter.convertRealToEuro(realAmount));
 
             double dollarAmount = 50;
 
-            System.out.println("Conversão de Dólar para Real: " + currencyConverter.convertDollarToReal(dollarAmount));
-            System.out.println("Conversão de Real para Dólar: " + currencyConverter.convertRealToDollar(realAmount));
+            System.out.println("Conversão de Dólar para Real: " + Converter.convertDollarToReal(dollarAmount));
+            System.out.println("Conversão de Real para Dólar: " + Converter.convertRealToDollar(realAmount));
 
         } catch (Exception e) {
             e.printStackTrace();
